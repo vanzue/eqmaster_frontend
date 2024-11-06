88720e503339a5d8e3b59d94c5f377ddf73bf421
wengcekai <153109702+wengcekai@users.noreply.github.com>
2024-11-06 17:16:50 +0800

    fixresultzh

---
+++ test/scripts/locate_name_zh.js
@@ -0,0 +1,36 @@
+export function findLastName(str) {
+	const regex = /(Sam|Anna)(?!.*(Sam|Anna))/;
+	const match = str.match(regex);
+
+	return match ? match[0] : 'Jason';
+}
+
+export function getAvatar(name) {
+	if (name == '小李') {
+		return '/static/npc1.png'
+	}
+	if (name == '小王') {
+		return '/static/npc2.png'
+	}
+	if (name == "Emma") {
+		return '/static/Emma.png'
+	}
+	if (name == "Bob") {
+		return '/static/Bob.png'
+	}
+	if (name == "Ryan") {
+		return '/static/Ryan.png'
+	}
+	if (name == "Colleague") {
+		return '/static/Colleague.png'
+	}
+	if (name == "Monica") {
+		return '/static/Monica.png'
+	}
+	if (name == "Alex") {
+		return '/static/Alex.png'
+	}
+
+
+	return '/static/npc3.png'
+}
