4b1234496d6eda537bbcf52bc5f79b46eb842163
v-tianzzhang <v-tianzzhang@microsoft.com>
2024-11-19 09:52:25 +0800

    修改为国内的azure地址

--- src/scripts/constants.js
+++ src/scripts/constants.js
@@ -1,5 +1,5 @@
-export const IMGURL="https://eqmaster.blob.core.windows.net";
-export const TOKEN="?sp=r&st=2024-10-30T07:28:21Z&se=2025-10-30T15:28:21Z&sv=2022-11-02&sr=c&sig=U3yHYWEQBvY3MnZO6kUh%2Fc0LiaLuvuPFp1YB4nnAL8E%3D";
+export const IMGURL="https://eqmaster.blob.core.chinacloudapi.cn";
+export const TOKEN="?sp=r&st=2024-11-18T09:41:26Z&se=2025-11-18T17:41:26Z&sv=2022-11-02&sr=c&sig=WL07d2l6cOkDXNTjNxkTEU3Yl0J%2FrNlWU%2FUPGJRPfhA%3D";
 export function getImg(picnName){
 	//console.log(IMGURL+picnName+TOKEN);
 	return IMGURL+picnName+TOKEN;
