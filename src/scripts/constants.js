940a1c6d7d0b29d3aa02f94a0e1ce90fc00d7ec9
Daniel Dai <daniel@danieldai.com>
2024-11-20 10:58:48 +0800

    change jszip import method

--- src/scripts/constants.js
+++ src/scripts/constants.js
@@ -1,4 +1,4 @@
-const JSZip = require('../static/jszip.min.js');
+import JSZip from 'jszip';
 
 export const IMGURL="https://eqmaster.blob.core.chinacloudapi.cn";
 export const TOKEN="?sp=r&st=2024-11-18T09:41:26Z&se=2025-11-18T17:41:26Z&sv=2022-11-02&sr=c&sig=WL07d2l6cOkDXNTjNxkTEU3Yl0J%2FrNlWU%2FUPGJRPfhA%3D";
