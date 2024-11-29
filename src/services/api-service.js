c82ff62c8f691478a96994cdb0c00e818b5aa2a9
Daniel Dai <daniel@danieldai.com>
2024-11-29 15:52:16 +0800

    fix dim_name

--- src/services/api-service.js
+++ src/services/api-service.js
@@ -174,7 +174,7 @@ export default {
 		try {
 			console.info('mapdata@@@@@@@@:');
 			const response = await uni.request({
-				url: url.getUrl(`/get_battlefield_map/${1}?dim_name=掌控力`),
+				url: url.getUrl(`/get_battlefield_map/${1}?dim_name=self_regulation`),
 				// url: url.getUrl(`/get_battlefield_map/${userId}?dim_name=掌控力`),
 				method: 'GET'
 			});
